9fd549366fac8168847664da8d7c7c8412e4b3f0
Alok Bhardwaj <alokmamta9@gmail.com>
2026-08-24 17:00:33 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (55.43%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> result = new ArrayList<>();
+        char[] chars = s.toCharArray();
+
+        backtrack(chars, 0, result);
+
+        return result;
+
+        }
+
+    private void backtrack(char[] chars, int index, List<String> result) {
+        
+        //base case
+        if (index == chars.length) {
+            result.add(new String(chars));
+            return;
+        }
+
+        
+        if (Character.isDigit(chars[index])) {
+            backtrack(chars, index + 1, result);
+            return;
+        }
+
+        
+        chars[index] = Character.toLowerCase(chars[index]);
+        backtrack(chars, index + 1, result);
+
+    
+        chars[index] = Character.toUpperCase(chars[index]);
+        backtrack(chars, index + 1, result);
+    
+        
+    }
+}
