de162e20679375ecca0efb43bd0f29ab0a4e59a1
Alok Bhardwaj <alokmamta9@gmail.com>
2026-08-31 16:42:18 +0530

    Time: 28 ms (7.47%) | Memory: 107 MB (71.85%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,17 @@
+import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i+1]) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+     for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+
+            mp.put(curr, mp.getOrDefault(curr, 0) + 1);
+
+            if (mp.get(curr) > 1) {
                 return true;
             }
-
         }
         return false;
-        
     }
 }
