21b548395185b06443a9f8baf7966fa82a9772b6
Alok Bhardwaj <alokmamta9@gmail.com>
2026-09-14 05:30:57 +0530

    Time: 7 ms (88.28%) | Memory: 44.3 MB (72.83%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int ans = 0;
+         int window = 0;
+         int left = 0;
+        for(int right = 0; right < n; right++) {
+        window += Math.abs(s.charAt(right)-t.charAt(right));
+        while(window > maxCost){
+            window -= Math.abs(s.charAt(left)-t.charAt(left));
+            left++;
+        }
+        ans=Math.max(ans, right-left+1);
+
+        }
+        return ans;
+
+         
+        
+    }
+}
